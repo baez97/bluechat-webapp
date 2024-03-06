3bd3150585b4358d5bb2975c5936944a8fcd1a75
José Manuel Báez <38162926+baez97@users.noreply.github.com>
2024-03-06 17:32:13 +0100

    Adding websocket support for graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import "./App.css";
 import { ChatScreen } from "./features/chat/screens/ChatScreen";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-const client = new ApolloClient({
+import { split, HttpLink } from "@apollo/client";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+
+const httpLink = new HttpLink({
   uri: "http://localhost:8080/query",
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:8080/query",
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 function App() {
